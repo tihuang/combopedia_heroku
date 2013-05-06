62dd24bb5d485f43e40ecf458c826952933ef338
tihuang <tiffany.huang08@gmail.com>
2013-05-06 18:23:02 -0400

    urls change to serve static

--- combopedia/urls.py
+++ combopedia/urls.py
@@ -19,7 +19,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 
-if not settings.DEBUG:
-        urlpatterns += patterns('',
+    urlpatterns += patterns('',
                         (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
                             )
